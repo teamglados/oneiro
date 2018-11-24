264efc3d9ec2f1372dc093154dc23b677c209f0f
Ville Toiviainen <villej.toiviainen@gmail.com>
2018-11-24 12:29:14 +0200

    common template

---
+++ heatmap/utils.py
@@ -0,0 +1,10 @@
+GEOJSON_TEMPLATE = {
+    "type": "FeatureCollection",
+    "crs": {
+        "type": "name",
+        "properties": {
+            "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
+        }
+    },
+    "features": []
+}
