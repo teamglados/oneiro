04da1d1079bfe55c6a8412a7a5082945b8ad324b
Ville Toiviainen <villej.toiviainen@gmail.com>
2018-11-24 19:17:28 +0200

    Try catch, faster photo frequency

--- raspberry/photographer.py
+++ raspberry/photographer.py
@@ -4,16 +4,19 @@ import requests
 from picamera import PiCamera
 
 camera = PiCamera()
-camera.resolution = (1024, 768)
+camera.resolution = (640, 480)
 camera.start_preview()
 
 url = os.getenv("API_URL", "https://api.teamglados.com/postimage")
 
 image_name = "pi_image.jpg"
 while True:
-    time.sleep(2)
+    time.sleep(0.5)
     camera.capture(image_name)
 
     files = {'media': open(image_name, 'rb')}
-    requests.post(url, files=files)
-    print(time.strftime('%Y-%m-%d %H:%M'), "-- uploded image")
+    try:
+        requests.post(url, files=files, timeout=2)
+        print(time.strftime('%Y-%m-%d %H:%M'), "-- uploded image")
+    except:
+        pass
