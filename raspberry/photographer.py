a4a0977c615fddea888aa14fea5fe1e49f5f126f
Ville Toiviainen <villej.toiviainen@gmail.com>
2018-11-24 16:30:28 +0200

    log

--- raspberry/photographer.py
+++ raspberry/photographer.py
@@ -1,6 +1,6 @@
 import os
+import time
 import requests
-from time import sleep
 from picamera import PiCamera
 
 camera = PiCamera()
@@ -11,8 +11,9 @@ url = os.getenv("API_URL", "https://api.teamglados.com/postimage")
 
 image_name = "pi_image.jpg"
 while True:
-    sleep(2)
+    time.sleep(2)
     camera.capture(image_name)
 
     files = {'media': open(image_name, 'rb')}
     requests.post(url, files=files)
+    print(time.strftime('%Y-%m-%d %H:%M'), "-- uploded image")
